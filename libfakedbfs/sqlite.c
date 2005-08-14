0626d5985d3f8d09115e0a1cc429fbe9b0ffd438
dcp1990 <dcp1990>
2005-08-14 08:15:30 +0000

    Err, why was that %s?

--- libfakedbfs/sqlite.c
+++ libfakedbfs/sqlite.c
@@ -27,7 +27,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
-/* $Amigan: fakedbfs/libfakedbfs/sqlite.c,v 1.6 2005/08/14 08:07:49 dcp1990 Exp $ */
+/* $Amigan: fakedbfs/libfakedbfs/sqlite.c,v 1.7 2005/08/14 08:15:30 dcp1990 Exp $ */
 /* system includes */
 #include <string.h>
 #include <stdlib.h>
@@ -39,7 +39,7 @@
 /* us */
 #include <fakedbfs.h>
 
-RCSID("$Amigan: fakedbfs/libfakedbfs/sqlite.c,v 1.6 2005/08/14 08:07:49 dcp1990 Exp $")
+RCSID("$Amigan: fakedbfs/libfakedbfs/sqlite.c,v 1.7 2005/08/14 08:15:30 dcp1990 Exp $")
 
 
 int open_db(f)
@@ -249,10 +249,11 @@ int add_to_field_desc(f, tablename, name, alias, type, typen)
 			" enumname, otherfield) VALUES('%q', '%q', %d, %s%q%s, %s);",
 			tablename, name, alias, type, typen != NULL ? "'" : "",
 			typen != NULL ? typen : "NULL", typen != NULL ? "'" : "", othna);
+	fprintf(stderr, "sql %s\n", sql);
 	rc = sqlite3_exec(f->db, sql, NULL, NULL, &emsg);
 	sqlite3_free(sql);
 	if(rc != SQLITE_OK) {
-		ERR(die, "add_to_field_desc(f, \"%s\", \"%s\", \"%s\", \"%s\", \"%s\"): SQLite error after exec: %s", tablename, name, alias, (int)type, typen, emsg);
+		ERR(die, "add_to_field_desc(f, \"%s\", \"%s\", \"%s\", %d, \"%s\"): SQLite error after exec: %s", tablename, name, alias, (int)type, typen, emsg);
 		if(type == oenum)
 			free(othna);
 		sqlite3_free(emsg);
