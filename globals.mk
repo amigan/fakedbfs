af27342f453bee3402d650352893a8c3a72d4251
dcp1990 <dcp1990>
2005-09-23 00:27:35 +0000

    Debugging stuff

--- globals.mk
+++ globals.mk
@@ -1,9 +1,12 @@
-# $Amigan: fakedbfs/globals.mk,v 1.8 2005/09/22 23:21:34 caelian Exp $
+# $Amigan: fakedbfs/globals.mk,v 1.9 2005/09/23 00:27:35 dcp1990 Exp $
 CC=cc
 CPPFLAGS=-I../include -I/usr/local/include -DUNIX
 CFLAGS+=-g -Wall -ggdb -fPIC
 # -ansi
 LDFLAGS=-L/usr/local/lib
+.if defined(FREEDEBUG)
+CPPFLAGS+=-DFREEDEBUG
+.endif
 .if defined(DMALLOC)
 DMALLOC=yes
 CPPFLAGS+=-DDMALLOC
