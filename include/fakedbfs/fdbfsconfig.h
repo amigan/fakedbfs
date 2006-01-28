8939c1c1810e68538f3b9dffab0c69ffd7e75d6b
dcp1990 <dcp1990>
2006-01-28 22:35:33 +0000

    misc

--- include/fakedbfs/fdbfsconfig.h
+++ include/fakedbfs/fdbfsconfig.h
@@ -1,5 +1,5 @@
 /* configuration header */
-/* $Amigan: fakedbfs/include/fakedbfs/fdbfsconfig.h,v 1.3 2005/10/14 21:15:20 dcp1990 Exp $ */
+/* $Amigan: fakedbfs/include/fakedbfs/fdbfsconfig.h,v 1.4 2006/01/28 22:35:33 dcp1990 Exp $ */
 
 #ifndef HAVE_FDBFSCONFIG_H
 
@@ -31,4 +31,6 @@
 #define CRAWLFRAME_MAX		20 /* this takes a LOT of memory...this many crawlframe_t's per directory frame...it's just a tradeoff between being able to
 				      crawl more directories or using less memory....once the hybrid system is finished (depth and breadth), this will be
 				      less of an issue */
+#define BUSY_RETRIES		3	/* number of times to retry upon a trappable SQLITE_BUSY exception...don't make this
+					   too big! */
 #endif
