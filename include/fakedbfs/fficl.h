59dc2cc42ae158ed9f64d979e9fa0bf87b8751b2
dcp1990 <dcp1990>
2006-01-30 20:59:53 +0000

    prototype

--- include/fakedbfs/fficl.h
+++ include/fakedbfs/fficl.h
@@ -27,7 +27,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
-/* $Amigan: fakedbfs/include/fakedbfs/fficl.h,v 1.2 2006/01/30 20:56:19 dcp1990 Exp $ */
+/* $Amigan: fakedbfs/include/fakedbfs/fficl.h,v 1.3 2006/01/30 20:59:53 dcp1990 Exp $ */
 /**
  * @file fficl.h
  * @brief fakedbfs ficl bindings.
@@ -117,5 +117,16 @@ int fdbfs_ficl_p_shutdown(ficlplug_t *fpl, char **errmsg);
  */
 fields_t* fdbfs_ficl_p_extract_from_file(ficlplug_t *fpl, char *filename,
 		char **errmsg);
+
+
+/**
+ * @brief Load ficlplugin and return a Plugin object.
+ *
+ * Parses the ficl plugin in the specified filename.
+ * @param f Fakedbfs instance.
+ * @param plugfile Filename to parse.
+ * @return Plugin object describing the plugin, or NULL on error.
+ */
+struct Plugin* fdbfs_ficl_load_ficlplugin(fdbfs_t *f, char *plugfile);
 
 #endif
