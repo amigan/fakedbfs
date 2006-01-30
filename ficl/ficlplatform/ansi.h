e65dfb8e505c53341524b1269cd53e206b3072eb
dcp1990 <dcp1990>
2006-01-30 21:18:05 +0000

    Reverting for 32bit

--- ficl/ficlplatform/ansi.h
+++ ficl/ficlplatform/ansi.h
@@ -2,15 +2,12 @@ typedef char ficlInteger8;
 typedef unsigned char ficlUnsigned8;
 typedef short ficlInteger16;
 typedef unsigned short ficlUnsigned16;
-typedef int ficlInteger32;
-typedef unsigned int ficlUnsigned32;
-typedef long ficlInteger64;
-typedef unsigned long ficlUnsigned64;
+typedef long ficlInteger32;
+typedef unsigned long ficlUnsigned32;
 
-typedef ficlInteger64 ficlInteger;
-typedef ficlUnsigned64 ficlUnsigned;
-typedef double ficlFloat;
-#define FICL_PLATFORM_ALIGNMENT (8)
+typedef ficlInteger32 ficlInteger;
+typedef ficlUnsigned32 ficlUnsigned;
+typedef float ficlFloat;
 
 #define FICL_PLATFORM_BASIC_TYPES   (1)
 #define FICL_PLATFORM_HAS_2INTEGER  (0)
