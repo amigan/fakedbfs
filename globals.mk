8f35dc2a7a8348ff2b78c88e5f15e733d9566b14
dcp1990 <dcp1990>
2006-12-09 00:27:43 +0000

    pthread needed for threaded sqlite3

--- globals.mk
+++ globals.mk
@@ -1,6 +1,6 @@
-# $Amigan: fakedbfs/globals.mk,v 1.12 2005/12/23 19:56:49 dcp1990 Exp $
+# $Amigan: fakedbfs/globals.mk,v 1.13 2006/12/09 00:27:43 dcp1990 Exp $
 CC=cc
 CPPFLAGS=-I../include -I/usr/local/include -I../ficl -DHAVE_CONFIG -DHAVE_FICL_H $(CPPOPTS)
 CFLAGS+=-g -Wall -ggdb -fPIC
 # -ansi
-LDFLAGS=-L/usr/local/lib $(LDEXT)
+LDFLAGS=-L/usr/local/lib $(LDEXT) -pthread
